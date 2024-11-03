d9c42b884027ae6f967d58bd7c39cbe66c2d148d
Vishnu <akulavishnuvardhanrdy@gmail.com>
2024-11-03 15:07:53 +0530

    Log Out Success

--- src/Components/Registrar/Sidebar.js
+++ src/Components/Registrar/Sidebar.js
@@ -1,37 +1,49 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import Logo from "../../Assets/images/Logo.png";
 import { FaUserPlus, FaSignOutAlt, FaUserMinus, FaFolderOpen, FaCheckCircle, FaCalendarAlt, FaPlusSquare, FaClock } from 'react-icons/fa';
 
-const Sidebar = () => (
-  <div className="bg-indigo-800 w-64 p-6 m-4 rounded-2xl shadow-lg flex flex-col text-white overflow-y-auto">
-    <div className="flex justify-center mb-2">
-      <img src={Logo} alt="logo" className="h-24 w-24" />
-    </div>
-    {menuItems.map(({ to, icon, label }) => (
-      <NavItem key={to} to={to} icon={icon} label={label} />
-    ))}
-  </div>
-);
+const Sidebar = () => {
+  const navigate = useNavigate();
 
-const NavItem = ({ icon, label, to }) => (
-  <Link to={to}>
-    <div className="flex items-center space-x-3 p-3 rounded-lg transition duration-200 ease-in-out hover:bg-indigo-700 hover:shadow-lg transform hover:scale-105 cursor-pointer">
-      <span className="text-gray-200 text-lg">{icon}</span>
-      <span className="text-gray-100 font-medium">{label}</span>
-    </div>
-  </Link>
-);
+  const handleLogout = () => {
+    sessionStorage.clear(); 
+    navigate('/login'); 
+  };
 
-const menuItems = [
-  { to: "add-account", icon: <FaUserPlus />, label: "Add Accounts" },
-  { to: "remove-account", icon: <FaUserMinus />, label: "Remove Accounts" },
-  { to: "create-court-case", icon: <FaPlusSquare />, label: "Add Case" },
-  { to: "schedule-court-case", icon: <FaCalendarAlt />, label: "Schedule Case" },
-  { to: "user-history", icon: <FaClock />, label: "User History" },
-  { to: "pending-court-cases", icon: <FaFolderOpen />, label: "Pending Cases" },
-  { to: "resolved-court-cases", icon: <FaCheckCircle />, label: "Resolved Cases" },
-  { to: "/", icon: <FaSignOutAlt />, label: "Log Out" }
-];
+  const NavItem = ({ icon, label, to, onClick }) => (
+    <Link to={to} onClick={onClick}>
+      <div className="flex items-center space-x-3 p-3 rounded-lg transition duration-200 ease-in-out hover:bg-indigo-700 hover:shadow-lg transform hover:scale-105 cursor-pointer">
+        <span className="text-gray-200 text-lg">{icon}</span>
+        <span className="text-gray-100 font-medium">{label}</span>
+      </div>
+    </Link>
+  );
+  
+  const menuItems = [
+    { to: "add-account", icon: <FaUserPlus />, label: "Add Accounts" },
+    { to: "remove-account", icon: <FaUserMinus />, label: "Remove Accounts" },
+    { to: "create-court-case", icon: <FaPlusSquare />, label: "Add Case" },
+    { to: "schedule-court-case", icon: <FaCalendarAlt />, label: "Schedule Case" },
+    { to: "user-history", icon: <FaClock />, label: "User History" },
+    { to: "pending-court-cases", icon: <FaFolderOpen />, label: "Pending Cases" },
+    { to: "resolved-court-cases", icon: <FaCheckCircle />, label: "Resolved Cases" },
+    { to: "/", icon: <FaSignOutAlt />, label: "Log Out", onClick: () => handleLogout() }
+  ];
+
+  
+  return (
+    <div className="bg-indigo-800 w-64 p-6 m-4 rounded-2xl shadow-lg flex flex-col text-white overflow-y-auto">
+      <div className="flex justify-center mb-2">
+        <img src={Logo} alt="logo" className="h-24 w-24" />
+      </div>
+      {menuItems.map(({ to, icon, label, onClick }) => (
+        <NavItem key={to} to={to} icon={icon} label={label} onClick={onClick} />
+      ))}
+    </div>
+  );
+};
+
+
 
 export default Sidebar;
